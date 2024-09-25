e265b410cfd8f68ef7dc136473cea2051b7e0475
praja09 <dipaprajadpp@gmail.com>
2024-09-25 14:36:24 +0700

    Praktikum 7 percobaan 2

---
+++ Jobsheet 5/string2.php
@@ -0,0 +1,13 @@
+<?php
+echo "Baris\nbaru <br>";
+echo 'Baris\nbaru <br>';
+echo "Halo\rDunia <br>";
+echo 'Halo\rDunia <br>';
+
+echo "<pres>Halo\tDunia!</pres>";
+echo '<pres>Halo\tDunia!</pres>';
+
+echo "Katakanlah \"Tidak pada narkoba!\" <br>";
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>';
+
+?>

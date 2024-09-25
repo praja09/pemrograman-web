d137277315bfbf0ea78e9d2a1dd9a0827c7d81c6
praja09 <dipaprajadpp@gmail.com>
2024-09-25 13:03:46 +0700

    Praktikum 4 nomor 10

---
+++ Jobsheet 5/function_return.php
@@ -0,0 +1,11 @@
+<?php
+//nomor 10
+//membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah ". hitungUmur(2005, 2024) ."tahun";
+
+?>

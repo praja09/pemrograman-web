c6a39dfca43eae844eecaaecf117246317f26b03
praja09 <dipaprajadpp@gmail.com>
2024-09-25 12:42:53 +0700

    Praktikum 4 nomor 4

--- Jobsheet 5/function.php
+++ Jobsheet 5/function.php
@@ -1,10 +1,29 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Dipa Praja<br/>";
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Dipa Praja<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
+// //memanggil fungsi yang sudah dibuat
+// perkenalan();
+// perkenalan();
+
+
+//nomor 4
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
+
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Hamdana","Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>

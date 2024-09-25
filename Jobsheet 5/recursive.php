bc9e07d83e36787d5f04ba50ea85893d23aeb090
praja09 <dipaprajadpp@gmail.com>
2024-09-25 13:19:46 +0700

    Praktikum 5 percobaan 3

--- Jobsheet 5/recursive.php
+++ Jobsheet 5/recursive.php
@@ -8,7 +8,18 @@
 // tampilkanHaloDunia();
 
 //percobaan 2
-for ($i=1; $i <=25; $i++) {
-    echo "Perulangan ke-{$i} <br>";
+// for ($i=1; $i <=25; $i++) {
+//     echo "Perulangan ke-{$i} <br>";
+// }
+
+//percobaan 3
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
+tampilkanAngka(20);
 ?>

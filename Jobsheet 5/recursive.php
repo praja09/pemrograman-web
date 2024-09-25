86215828e8ecbe09c7855ca1447c3b58366ea36d
praja09 <dipaprajadpp@gmail.com>
2024-09-25 13:17:02 +0700

    Praktikum 5 percobaan 2

--- Jobsheet 5/recursive.php
+++ Jobsheet 5/recursive.php
@@ -1,8 +1,14 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
+//percobaan 1
+// function tampilkanHaloDunia(){
+//     echo "Halo dunia! <br>";
 
-    tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+// tampilkanHaloDunia();
+
+//percobaan 2
+for ($i=1; $i <=25; $i++) {
+    echo "Perulangan ke-{$i} <br>";
 }
-tampilkanHaloDunia();
 ?>

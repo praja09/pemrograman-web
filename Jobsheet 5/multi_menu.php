b5a0e00685649c4ffce7474c53b5cfad708c5edd
praja09 <dipaprajadpp@gmail.com>
2024-09-25 14:03:11 +0700

    Praktikum 6 percobaan 1

---
+++ Jobsheet 5/multi_menu.php
@@ -0,0 +1,36 @@
+<?php
+// percobaan 1
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama"=> "Berita",
+        "subMenu"=> [
+            [
+                "nama" => "Wisata",
+                "subMenu"=> [
+                    [
+                        "nama"=> "Pantai"
+                    ],
+                    [
+                        "nama"=> "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama"=> "Kuliner"
+            ],
+            [
+                "nama"=> "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama"=> "Tentang"
+    ],
+    [
+        "nama"=> "Kontak"
+    ],
+];
+?>

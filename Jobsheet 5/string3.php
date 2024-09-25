c2bffc2bacdedd33240b6ca6a8196ca060b0d8d3
praja09 <dipaprajadpp@gmail.com>
2024-09-25 14:51:13 +0700

    Praktikum 7 percobaan 4

--- Jobsheet 5/string3.php
+++ Jobsheet 5/string3.php
@@ -1,6 +1,13 @@
 <?php
 
 $pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
+// echo strrev($pesan) . "<br>";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerkata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerkata = array_map(fn($pesan)=> strrev($pesan), $pesanPerkata);
+# gbungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerkata);
 
+echo $pesan . "<br>";
 ?>

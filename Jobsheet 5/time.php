3572bf3094ca97364225afe8cfcef71d2a92f5ed
praja09 <dipaprajadpp@gmail.com>
2024-09-25 15:41:40 +0700

    Praktikum 10 percobaan 2 (ini benar)

--- Jobsheet 5/time.php
+++ Jobsheet 5/time.php
@@ -5,7 +5,7 @@
     <body>
         <h3> Time </h3>
         <?php
-            date_default_timezone_get("asia/jakarta");
+            date_default_timezone_set("asia/jakarta");
             echo date("h:i:sa");
         ?>
     </body>

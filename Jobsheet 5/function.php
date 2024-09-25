ffcffb6bd0a2a4c82906aeed1f9bc475c7464a59
praja09 <dipaprajadpp@gmail.com>
2024-09-25 12:49:48 +0700

    Praktikum 4 nomor 7

--- Jobsheet 5/function.php
+++ Jobsheet 5/function.php
@@ -10,10 +10,29 @@
 
 
 //nomor 4
-function perkenalan($nama, $salam){
+// function perkenalan($nama, $salam){
+//     echo $salam.", ";
+//     echo "Perkenalkan, nama saya ".$nama."<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
+
+// //memanggil fungsi yang sudah dibuat
+// perkenalan("Hamdana","Hallo");
+
+// echo "<hr>";
+
+// $saya = "Elok";
+// $ucapanSalam = "Selamat pagi";
+
+// //memanggil lagi
+// perkenalan($saya, $ucapanSalam);
+
+
+//nomor 7
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+    echo "Senang berkenlan dengan Anda<br/>";
 }
 
 //memanggil fungsi yang sudah dibuat
@@ -24,6 +43,6 @@ echo "<hr>";
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 
-//memanggil lagi
-perkenalan($saya, $ucapanSalam);
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
 ?>

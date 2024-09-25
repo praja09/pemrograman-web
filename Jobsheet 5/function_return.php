041a9590818db41c283f4d357ee378b3d5ce0f48
praja09 <dipaprajadpp@gmail.com>
2024-09-25 13:04:41 +0700

    Praktikum 4 nomor 13

--- Jobsheet 5/function_return.php
+++ Jobsheet 5/function_return.php
@@ -1,11 +1,28 @@
 <?php
 //nomor 10
 //membuat fungsi
+// function hitungUmur($thn_lahir, $thn_sekarang){
+//     $umur = $thn_sekarang - $thn_lahir;
+//     return $umur;
+// }
+
+// echo "Umur saya adalah ". hitungUmur(2005, 2024) ."tahun";
+
+
+//nomor 13
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.",";
+    echo " Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2005, 2024) ."tahun";
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2005, 2024) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
 
+//memanggil fungsi perkenalan
+    perkenalan("Elok");
 ?>

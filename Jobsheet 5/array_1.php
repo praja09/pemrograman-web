676ab923964f1f9b7639047275dec69df1ad62df
praja09 <dipaprajadpp@gmail.com>
2024-09-25 10:18:51 +0700

    praktikum 2

--- Jobsheet 5/array_1.php
+++ Jobsheet 5/array_1.php
@@ -3,13 +3,15 @@
     <head>
     </head>
     <body>
-        <h2>Array Terindeks</h2>
+        <h2>Array Terindeks Dengan Loop</h2>
         <?php
             $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
-
-            echo $Listdosen[2] . "<br>";
-            echo $Listdosen[0] . "<br>";
-            echo $Listdosen[1] . "<br>";
-        // ?>
+            foreach ($Listdosen as $dosen) {
+                echo $dosen . "<br>";
+            // echo $Listdosen[2] . "<br>";
+            // echo $Listdosen[0] . "<br>";
+            // echo $Listdosen[1] . "<br>";
+            }
+        ?>
     </body>
 </html>

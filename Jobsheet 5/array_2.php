eff86fc9f30aa15f47dea354e376db9b9b8fbf69
praja09 <dipaprajadpp@gmail.com>
2024-09-25 10:20:37 +0700

    Praktikum 2

--- Jobsheet 5/array_2.php
+++ Jobsheet 5/array_2.php
@@ -2,18 +2,25 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device=width, initial-scale=1">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Data Dosen</title>
+        <link rel="stylesheet" href="style1.css">
     </head>
     <body>
-        <?php
-            $Dosen = [
-                'nama' => 'Elok Nur Hamdana',
-                'domisili' => 'Malang',
-                'jenis_kelamin' => 'Perempuan' ];
-            echo "Nama : {$Dosen ['nama']} <br>";
-            echo "Domisili : {$Dosen ['domisili']} <br>";
-            echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
-        ?>
+        <table>
+            <tr><th>Field</th><th>Value</th></tr>
+            <?php
+                $Dosen = [
+                    'nama' => 'Elok Nur Hamdana',
+                    'domisili' => 'Malang',
+                    'jenis_kelamin' => 'Perempuan' 
+                ];
+
+                // Menampilkan data dosen dalam bentuk tabel
+                echo "<tr><td>Nama</td><td>{$Dosen['nama']}</td></tr>";
+                echo "<tr><td>Domisili</td><td>{$Dosen['domisili']}</td></tr>";
+                echo "<tr><td>Jenis Kelamin</td><td>{$Dosen['jenis_kelamin']}</td></tr>";
+            ?>
+        </table>
     </body>
 </html>

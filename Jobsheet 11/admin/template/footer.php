e419752dd251170c8774937328555e2d4c77d73e
praja09 <dipaprajadpp@gmail.com>
2024-11-06 14:27:43 +0700

    Jobsheet 11 Praktikum 3

---
+++ Jobsheet 11/admin/template/footer.php
@@ -0,0 +1,7 @@
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js" integrity="sha384-SR9zKtIRQt0rrXTyz+xc/qpH7ztIzhTuLTLQzLnf9kY1YJTjNM9FMN9fBaZq9gUJ" crossorigin="anonymous"></script>
+<script src="assets/custom/dashboard.js"></script>
+</body>
+
+</html>
---
+++ Jobsheet 11/admin/template/home.php
@@ -0,0 +1,41 @@
+<div class="container-fluid">
+    <div class="row">
+        <?php
+        include 'menu.php';
+
+        $query_anggota = "SELECT COUNT(id) AS Jumlah FROM anggota";
+        $result_anggota = mysqli_query($koneksi, $query_anggota);
+        $row_anggota = mysqli_fetch_assoc($result_anggota);
+
+        $query_jabatan = "SELECT COUNT(id) AS Jumlah FROM jabatan";
+        $result_jabatan = mysqli_query($koneksi, $query_jabatan);
+        $row_jabatan = mysqli_fetch_assoc($result_jabatan);
+        ?>
+
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Dashboard</h1>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Jumlah Anggota</h5>
+                            <p class="card-text"><?php echo $row_anggota['Jumlah']; ?> orang</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Jumlah Jabatan</h5>
+                            <p class="card-text"><?php echo $row_jabatan['Jumlah']; ?> orang</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>

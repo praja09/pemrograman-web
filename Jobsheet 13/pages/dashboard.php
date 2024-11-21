e4224549c417583a8d77ad0f22008abb2cdee0c8
praja09 <dipaprajadpp@gmail.com>
2024-11-21 18:07:41 +0700

    Jobsheet 13 Modif

--- Jobsheet 13/pages/dashboard.php
+++ Jobsheet 13/pages/dashboard.php
@@ -1,59 +1,46 @@
-<?php
-// Cek apakah pengguna sudah login
-if (!$session->get('is_login')) {
-    header('Location: ../login.php');
-    exit();
-}
-// Ambil nama pengguna dari session
-$level = $session->get('level');
-$nama = $session->get('name');
-?>
+<!-- Content Header (Page header) -->
 <section class="content-header">
-      <div class="container-fluid">
+    <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
-          </div>
+            <div class="col-sm-6">
+                <h1>Dashboard</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </div>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+    </div><!-- /.container-fluid -->
+</section>
 
-    <!-- Main content -->
-    <section class="content">
+<!-- Main content -->
+<section class="content">
 
-      <!-- Default box -->
-      <div class="card">
+    <!-- Default box -->
+    <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Selamat Datang</h3>
+            <h3 class="card-title">Selamat Datang</h3>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
         </div>
         <div class="card-body">
-            <!-- modifikasi untuk menampilkan nama dan level -->
-            <p>
-                Selamat datang <b><?php echo htmlspecialchars($nama);?></b>,
-                Anda Login Sebagai <b><?php echo htmlspecialchars($level); ?></b>
-            </p>
+            Selamat datang Administrator Anda login sebagai admin
         </div>
         <!-- /.card-body -->
-        <!-- <div class="card-footer">
-          Footer
-        </div> -->
+        <div class="card-footer">
+        </div>
         <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
+    </div>
+    <!-- /.card -->
 
-    </section>
+</section>
+<!-- /.content -->

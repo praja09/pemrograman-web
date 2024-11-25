97088f0ffa650b77539b627ae48090107d6c42d4
praja09 <dipaprajadpp@gmail.com>
2024-11-25 16:15:15 +0700

    Jobsheet 13 MODIF

--- Jobsheet 13/lib/Connection.php
+++ Jobsheet 13/lib/Connection.php
@@ -1,13 +1,36 @@
-<?php
-$username = 'root';
-$password = '';
-$database = 'dasar_web';
-try{
-    $db = new mysqli('localhost', $username, $password, $database);
-    if($db->connect_error){
-        die('Connection DB failed: ' . $db->connect_error);
-    }
-}catch(Exception $e){
-    die($e->getMessage());
-}
-?>
+<?php  
+    $use_driver = 'sqlsrv'; // mysql atau sqlsrv 
+ 
+    $host     = "DIPA-PRAJA"; 
+    $username = ''; //'sa'; 
+    $password = ''; 
+    $database = 'dasar_web'; 
+    $db; 
+ 
+    if($use_driver == 'mysql'){    
+        try{ 
+            $db = new mysqli('localhost', $username, $password, $database); 
+            if($db->connect_error){ 
+                die('Connection DB failed: ' . $db->connect_error); 
+            } 
+        }catch(Exception $e){ 
+            die($e->getMessage()); 
+        } 
+    } else if($use_driver == 'sqlsrv'){ 
+        $credential = [ 
+            'Database' => $database,  
+            'UID' => $username,  
+            'PWD' => $password 
+        ]; 
+         
+        try{ 
+            $db = sqlsrv_connect($host, $credential); 
+ 
+            if (!$db){ 
+                $msg = sqlsrv_errors(); 
+                die($msg[0]['message']); 
+            } 
+        }catch(Exception $e){ 
+            die($e->getMessage()); 
+        } 
+    } 
